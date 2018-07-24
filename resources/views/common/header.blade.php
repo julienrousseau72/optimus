8875636596a27f384e0bb0d010b989d624eef8f1
Julien Rousseau <julien.rousseau72@gmail.com>
2018-07-24 22:24:46 +0200

    Rangement dossier

---
+++ resources/views/common/header.blade.php
@@ -0,0 +1,41 @@
+<header>
+    <h1>
+         <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'On parie quoi ?') }}
+        </a>
+    </h1>
+    <nav>
+        <ul>
+            <li><a href="/">Accueil</a></li>
+            <li>Parie</li>
+            <li>Contact</li>
+            @auth
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ url('/home') }}">Home</a>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @else
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+            @endauth
+        </ul>
+    </nav>
+</header>
